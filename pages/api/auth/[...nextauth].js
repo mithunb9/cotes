01aa4cad2a4c9f4cc142588ee3587d1cdbddebb6
My-Name-Ae-Jeffv1 <51266456+My-Name-Ae-Jeffv1@users.noreply.github.com>
2022-09-17 09:40:05 -0500

    add login system with github app

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import './components/accessToken';
+import "./components/accessToken";
 
 export default NextAuth({
   providers: [
