ac7b7a7c8623b926eb634c976b91d0b4b6463d2d
My-Name-Ae-Jeffv1 <51266456+My-Name-Ae-Jeffv1@users.noreply.github.com>
2022-09-17 11:56:57 -0500

    initialize firebas

--- pages/index.js
+++ pages/index.js
@@ -4,37 +4,56 @@ import styles from "../styles/Home.module.css";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Button, Icon, IconButton } from "@mui/material";
+import { firestore } from "../firebase/firebase";
+import {
+  collection,
+  QueryDocumentSnapshot,
+  DocumentData,
+  query,
+  where,
+  limit,
+  getDocs,
+} from "firebase/firestore";
+import { useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-      Signed in as {session.user.email} <br />
-      <button onClick={() => signOut()}>Sign out</button>
-      <div className={styles.container}>
-        <CodeMirror
-          value="console.log('hello world!');"
-          height="200px"
-          theme="dark"
-          extensions={[javascript({ jsx: true })]}
-        />
+        Signed in as {session.user.email} <br />
+        <Button variant="contained" onClick={() => signOut()}>
+          Sign out
+        </Button>
+        <div className={styles.container}>
+          <CodeMirror
+            value="console.log('hello world!');"
+            height="200px"
+            theme="dark"
+            extensions={[javascript({ jsx: true })]}
+          />
 
-        <Head>
-          <title>Cotes</title>
-          <meta name="description" content="Note Taking App for Programmers" />
-        </Head>
+          <Head>
+            <title>Cotes</title>
+            <meta
+              name="description"
+              content="Note Taking App for Programmers"
+            />
+          </Head>
 
-        <main className={styles.main}></main>
-      </div>
+          <main className={styles.main}></main>
+        </div>
       </>
     );
   }
 
   return (
     <>
-    Not Signed in <br />
-    <button onClick={() => signIn()}>Sign in</button>
+      Not Signed in <br />
+      <Button variant="contained" onClick={() => signIn()}>
+        Sign in
+      </Button>
     </>
   );
 }
