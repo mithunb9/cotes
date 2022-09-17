3fd3aa694a147e32744739b08ee4d354bd653207
My-Name-Ae-Jeffv1 <51266456+My-Name-Ae-Jeffv1@users.noreply.github.com>
2022-09-17 17:48:13 -0500

    add function to save notebook onto firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,89 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+
+const bruh = {
+  user: "pranavbalu",
+  files: [
+    {
+      type: "folder",
+      name: "Mithun",
+      contents: [],
+    },
+    {
+      type: "notebook",
+      name: "Bruh",
+      pages: [
+        {
+          title: "bruh",
+          content: [
+            { type: "heading", content: "Sample Heading" },
+            { type: "heading", content: "Sample Heading" },
+          ],
+        },
+      ],
+    },
+    {},
+  ],
+};
+const bruh2 = {
+  user: "mithunb",
+  files: [
+    {
+      type: "folder2",
+      name: "Mithun2",
+      contents: [],
+    },
+    {
+      type: "notebook2",
+      name: "Bruh2",
+      pages: [
+        {
+          title: "bruh2",
+          content: [
+            { type: "heading2", content: "Sample Heading2" },
+            { type: "heading2", content: "Sample Heading2" },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+
+const { initializeApp } = require("firebase/app");
+const { getDatabase } = require("firebase/database");
+const { getFirestore } = require("firebase/firestore");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const firebaseConfig = {
-        apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-
-}
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
 console.log("Firebase initialized");
 const app = initializeApp(firebaseConfig);
 
-const firestore = getFirestore();
-export {firestore};
+const db = getFirestore(app);
+
+const { collection, addDoc, getDocs } = require("firebase/firestore");
+
+const addNotebook = async (notebook) => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), notebook);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+
+//module.exports(addPost);
+//addNotebook(bruh);
+//addNotebook(bruh2);
+
