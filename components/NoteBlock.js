ccccbd7f095c111f01c12a46d004af3842069733
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-09-18 06:03:25 -0500

    fix styling

--- components/NoteBlock.js
+++ components/NoteBlock.js
@@ -41,8 +41,10 @@ export default function NoteBlock(props) {
           extensions={[javascript({ jsx: true })]}
           onChange={onChange}
         />
+        <div>
+          <p>{output}</p>
+        </div>
         <button onClick={runCode}>Run</button>
-        <p>{output}</p>
       </div>
     );
   }
