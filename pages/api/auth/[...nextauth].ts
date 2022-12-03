425b99de555456da1ca7275097fff9d912515381
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2022-12-02 19:22:48 -0600

    update API and add authentication API

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,9 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongo";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GithubProvider({
       // @ts-ignore
@@ -11,7 +12,22 @@ export default NextAuth({
       // @ts-ignore
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      // @ts-ignore
+      clientId: process.env.GOOGLE_ID,
+      // @ts-ignore
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   secret: process.env.NEXTAUTH_SECRET,
-});
+  callbacks: {
+    async session(session, user) {
+      session.id = session.user.id;
+      return session;
+    },
+  },
+};
+
+// @ts-ignore
+export default NextAuth(authOptions);
--- pages/api/get.ts
+++ pages/api/get.ts
@@ -1,6 +1,16 @@
 import clientPromise from "../../lib/mongo";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req, res) {
+  // @ts-ignore
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
   if (req.method === "GET") {
     if (req.query.user === undefined) {
       res.status(200).json({ error: "User not found" });
@@ -8,7 +18,7 @@ export default async function handler(req, res) {
       const client = await clientPromise;
       const db = client.db("test");
       const collection = db.collection("users");
-      const user = await collection.findOne({ email: req.query.email });
+      const user = await collection.findOne({ _id: req.query.id });
 
       console.log(user);
       res.status(200).json(user);
